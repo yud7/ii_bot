318c347d03cb6b8fe5a228a7714ed3b2e1dacfd3
Zuev <zuevart.work@gmail.com>
2025-01-15 16:05:48 +0300

    cleared master

--- utils/database.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-FILE_PATH = "data/users.csv"
-
-def save_user_data(user_data: dict):
-    with open(FILE_PATH, mode="a", newline="", encoding="utf-8") as file:
-        writer = csv.writer(file)
-        writer.writerow([user_data["name"], user_data["age"], user_data["email"]])
